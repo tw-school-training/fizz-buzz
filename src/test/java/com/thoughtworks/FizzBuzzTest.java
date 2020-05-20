b441b849ee9685c2e8165997efbc8285b2d65ab5
Zhaoyu <weixiaobo88@gmail.com>
2020-05-20 14:16:15 +0800

    [Linne] - extract method for check expectation and actual result to eliminate duplication

--- src/test/java/com/thoughtworks/FizzBuzzTest.java
+++ src/test/java/com/thoughtworks/FizzBuzzTest.java
@@ -6,311 +6,144 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class FizzBuzzTest {
-
-    @Test
-    public void should_return_normal_number_given_normal_number() {
+    private void checkFizzBuzzResult(int number, String result) {
         //given
         FizzBuzz fizzBuzz = new FizzBuzz();
 
         //when
-        String result = fizzBuzz.say(1);
+        String actual = fizzBuzz.say(number);
 
         //then
-        assertThat(result, is("1"));
+        assertThat(actual, is(result));
+    }
+
+    @Test
+    public void should_return_normal_number_given_normal_number() {
+        checkFizzBuzzResult(1, "1");
     }
 
     @Test
     public void should_return_fizz_given_number_can_be_divided_by_3() {
-        //given
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
-        //when
-        String result = fizzBuzz.say(3);
-
-        //then
-        assertThat(result, is("Fizz"));
+        checkFizzBuzzResult(3, "Fizz");
     }
 
     @Test
     public void should_return_buzz_given_number_can_be_divided_by_5() {
-        //given
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
-        //when
-        String result = fizzBuzz.say(5);
-
-        //then
-        assertThat(result, is("Buzz"));
+        checkFizzBuzzResult(5, "Buzz");
     }
 
     @Test
     public void should_return_whizz_given_number_can_be_divided_by_7() {
-        //given
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
-        //when
-        String result = fizzBuzz.say(7);
-
-        //then
-        assertThat(result, is("Whizz"));
+        checkFizzBuzzResult(7, "Whizz");
     }
 
     @Test
     public void should_return_fizzbuzz_given_number_can_be_divided_by_3_and_5() {
-        //given
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
-        //when
-        String result = fizzBuzz.say(60);
-
-        //then
-        assertThat(result, is("FizzBuzz"));
+        checkFizzBuzzResult(60, "FizzBuzz");
     }
 
     @Test
     public void should_return_fizzwhizz_given_number_can_be_divided_by_3_and_7() {
-        //given
-        FizzBuzz fizzBuzz = new FizzBuzz();
+        checkFizzBuzzResult(21, "FizzWhizz");
 
-        //when
-        String result = fizzBuzz.say(21);
-
-        //then
-        assertThat(result, is("FizzWhizz"));
     }
 
     @Test
     public void should_return_buzzwhizz_given_number_can_be_divided_by_5_and_7() {
-        //given
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
-        //when
-        String result = fizzBuzz.say(140);
-
-        //then
-        assertThat(result, is("BuzzWhizz"));
+        checkFizzBuzzResult(140, "BuzzWhizz");
     }
 
     @Test
     public void should_return_fizzbuzzwhizz_given_number_can_be_divided_by_3_and_5_and_7() {
-        //given
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
-        //when
-        String result = fizzBuzz.say(210);
-
-        //then
-        assertThat(result, is("FizzBuzzWhizz"));
+        checkFizzBuzzResult(210, "FizzBuzzWhizz");
     }
 
     @Test
     public void should_return_fizz_given_number_contains_3_and_can_not_be_divided_by_all_of_3_and_5_and_7() {
-        //given
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
-        //when
-        String result = fizzBuzz.say(13);
-
-        //then
-        assertThat(result, is("Fizz"));
+        checkFizzBuzzResult(13, "Fizz");
     }
 
     @Test
     public void should_return_fizz_given_number_contains_3_and_can_be_divided_by_3_and_5_and_7() {
-        //given
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
-        //when
-        String result = fizzBuzz.say(630);
-
-        //then
-        assertThat(result, is("Fizz"));
+        checkFizzBuzzResult(630, "Fizz");
     }
 
     @Test
     public void should_return_fizz_given_number_contains_3_and_can_be_divided_by_both_of_3_and_5_but_not_divided_by_7() {
-        //given
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
-        //when
-        String result = fizzBuzz.say(30);
-
-        //then
-        assertThat(result, is("Fizz"));
+        checkFizzBuzzResult(30, "Fizz");
     }
 
     @Test
     public void should_return_fizz_given_number_contains_3_and_can_be_divided_by_both_of_3_and_7_but_not_divided_by_5() {
-        //given
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
-        //when
-        String result = fizzBuzz.say(63);
-
-        //then
-        assertThat(result, is("Fizz"));
+        checkFizzBuzzResult(63, "Fizz");
     }
 
     @Test
     public void should_return_fizz_given_number_contains_3_and_can_be_divided_by_both_of_5_and_7_but_not_divided_by_3() {
-        //given
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
-        //when
-        String result = fizzBuzz.say(1330);
-
-        //then
-        assertThat(result, is("Fizz"));
+        checkFizzBuzzResult(1330, "Fizz");
     }
 
     @Test
     public void should_return_fizz_given_number_contains_3_and_can_be_divided_by_3_but_not_divided_by_either_5_or_7() {
-        //given
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
-        //when
-        String result = fizzBuzz.say(9);
-
-        //then
-        assertThat(result, is("Fizz"));
+        checkFizzBuzzResult(9, "Fizz");
     }
 
     @Test
     public void should_return_fizz_given_number_contains_3_and_can_be_divided_by_5_but_not_divided_by_either_3_or_7() {
-        //given
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
-        //when
-        String result = fizzBuzz.say(310);
-
-        //then
-        assertThat(result, is("Fizz"));
+        checkFizzBuzzResult(310, "Fizz");
     }
 
     @Test
     public void should_return_fizz_given_number_contains_3_and_can_be_divided_by_7_but_not_divided_by_either_3_or_5() {
-        //given
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
-        //when
-        String result = fizzBuzz.say(364);
-
-        //then
-        assertThat(result, is("Fizz"));
+        checkFizzBuzzResult(364, "Fizz");
     }
 
 
     @Test
     public void should_return_the_number_given_number_contains_5_and_can_not_be_divided_by_either_5_or_7() {
-        //given
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
-        //when
-        String result = fizzBuzz.say(51);
-
-        //then
-        assertThat(result, is("51"));
+        checkFizzBuzzResult(51, "51");
     }
 
     @Test
     public void should_return_buzzwhizz_given_number_contains_5_and_can_be_divided_by_both_of_5_and_7() {
-        //given
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
-        //when
-        String result = fizzBuzz.say(35);
-        String secondResult = fizzBuzz.say(105);
-        String thirdResult = fizzBuzz.say(315);
-
-        //then
-        assertThat(result, is("BuzzWhizz"));
-        assertThat(secondResult, is("BuzzWhizz"));
-        assertThat(thirdResult, is("BuzzWhizz"));
+        checkFizzBuzzResult(35, "BuzzWhizz");
+        checkFizzBuzzResult(105, "BuzzWhizz");
+        checkFizzBuzzResult(315, "BuzzWhizz");
     }
 
     @Test
     public void should_return_buzz_given_number_contains_5_and_can_be_divided_by_5_but_not_divided_by_7() {
-        //given
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
-        //when
-        String result = fizzBuzz.say(15);
-        String secondResult = fizzBuzz.say(305);
-
-        //then
-        assertThat(result, is("Buzz"));
-        assertThat(secondResult, is("Buzz"));
+        checkFizzBuzzResult(15, "Buzz");
+        checkFizzBuzzResult(305, "Buzz");
     }
 
     @Test
     public void should_return_buzz_given_number_contains_5_and_can_be_divided_by_7_but_not_divided_by_5() {
-        //given
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
-        //when
-        String result = fizzBuzz.say(56);
-
-        //then
-        assertThat(result, is("Whizz"));
+        checkFizzBuzzResult(56, "Whizz");
     }
 
     @Test
     public void should_return_the_number_given_number_contains_7_but_can_not_be_divided_by_either_3_or_7() {
-        //given
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
-        //when
-        String result = fizzBuzz.say(17);
-
-        //then
-        assertThat(result, is("17"));
+        checkFizzBuzzResult(17, "17");
     }
 
     @Test
     public void should_return_fizzwhizz_given_number_contains_7_and_can_be_divided_by_all_of_3_and_5_and_7() {
-        //given
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
-        //when
-        String result = fizzBuzz.say(735);
-
-        //then
-        assertThat(result, is("FizzWhizz"));
+        checkFizzBuzzResult(735, "FizzWhizz");
     }
 
     @Test
     public void should_return_fizzwhizz_given_number_contains_7_and_can_be_divided_by_either_3_or_7() {
-        //given
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
-        //when
-        String result = fizzBuzz.say(147);
-
-        //then
-        assertThat(result, is("FizzWhizz"));
+        checkFizzBuzzResult(147, "FizzWhizz");
     }
 
     @Test
     public void should_return_fizz_given_number_contains_7_and_can_be_divided_by_3_but_not_divided_by_7() {
-        //given
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
-        //when
-        String result = fizzBuzz.say(75);
-
-        //then
-        assertThat(result, is("Fizz"));
+        checkFizzBuzzResult(75, "Fizz");
     }
 
     @Test
     public void should_return_whizz_given_number_contains_7_and_can_be_divided_by_7_but_not_divided_by_3() {
-        //given
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
-        //when
-        String result = fizzBuzz.say(70);
-
-        //then
-        assertThat(result, is("Whizz"));
+        checkFizzBuzzResult(70, "Whizz");
     }
 }
